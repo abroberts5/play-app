ed67adc170c8047447c38a83eefd41a8daef3188
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-03-23 13:26:57 -0600

    update to knex database for heroku build, missing migration info
    Co-authored-by: Nick Lindeberg <lindebergnick@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,13 @@
 module.exports = {
   production: {
     client: 'postgresql',
-    connection: 'postgres://piwipkjrpwuppz:907ed7363c245ad91b95850fec161cad6977d114c4351a299b911a559bd660c9@ec2-23-23-173-30.compute-1.amazonaws.com:5432/dtgm7h8okrdl3'
-  },
-  
+    connection: 'postgres://piwipkjrpwuppz:907ed7363c245ad91b95850fec161cad6977d114c4351a299b911a559bd660c9@ec2-23-23-173-30.compute-1.amazonaws.com:5432/dtgm7h8okrdl3',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+    },
+
   development: {
     client: 'pg',
     connection: 'postgres://localhost/play_app',
