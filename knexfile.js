60365d5dbc4498c6d6debfb6ddb9815022bfa681
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-03-23 13:18:09 -0600

    update to circle ci config test error changed, created production export in knexfile for heroku database build
    Co-authored-by: Nick Lindeberg <lindebergnick@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,9 @@
 // Update with your config settings.
 
 module.exports = {
+  production: {
+    client: 'postgresql',
+    connection: 'postgres://piwipkjrpwuppz:907ed7363c245ad91b95850fec161cad6977d114c4351a299b911a559bd660c9@ec2-23-23-173-30.compute-1.amazonaws.com:5432/dtgm7h8okrdl3'
 
   development: {
     client: 'pg',
